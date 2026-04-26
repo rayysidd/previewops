63f6bbf4580c1459d683528d9e0d988a8c6b03b9
shivenrshukla <shivenshukla297@gmail.com>
2026-04-26 17:33:02 +0530

    "Common js"

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>PreviewOps Environment #3 is Live!</h1>');
+  res.send('<h1>PreviewOps Environment is LIVE! 🚀</h1>');
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`App listening at http://0.0.0.0:${port}`);
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
